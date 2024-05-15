29fd64202c4a79e21029169405ed1c4979c32f5c
Kristian Perez <kpere192@fiu.edu>
2024-05-15 14:22:52 -0400

    Added 1 to Many relationship between posts and users; and some basic validators on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :poster, presence: true
+  validates :body, presence: true, length: { maximum: 1000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_175136) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_181422) do
+  create_table "posts", force: :cascade do |t|
+    t.string "poster"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
@@ -21,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_175136) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "posts", "users"
 end
